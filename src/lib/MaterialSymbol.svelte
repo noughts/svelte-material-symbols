18b222aea37b989bc0faab0e0aaea726f40d9d59
Koichi Yamamoto <noughts@users.noreply.github.com>
2024-07-07 23:31:56 +0900

    webfontではなく画像を表示するようにしました
    ロードするファイルサイズ削減のため

--- src/lib/MaterialSymbol.svelte
+++ src/lib/MaterialSymbol.svelte
@@ -1,15 +1,4 @@
 <script lang="ts">
-    /*
-
-    以下のタグを html に追加してください。
-
-    <link
-		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block"
-		rel="stylesheet" />
-
-    svelte:head を使って入れてみましたが、遅延によってレイアウトシフトが発生したので手動で入れましょう。
-
-    */
     export let icon: string;
     export let opacity: number = 1;
     export let fill: boolean = false;
@@ -17,20 +6,22 @@
     export let grade: -25 | 0 | 200 = 0;
     export let opticalSize: 20 | 24 | 40 | 48 = 24;
 
-    $: style = `
-        font-variation-settings:
-            'FILL' ${fill ? 1 : 0},
-            'wght' ${weight},
-            'GRAD' ${grade},
-            'opsz' ${opticalSize};
-        `;
+    $: options = [
+        weight == 400 ? undefined : `wght${weight}`,
+        grade == 0 ? undefined : `grad${grade}`,
+        fill == false ? undefined : `fill1`,
+    ].join("");
+    $: ary = [icon, options.length > 0 ? options : "", `${opticalSize}px`].filter(
+        (x) => x.length > 0,
+    );
+    $: filename = ary.join("_");
+    $: src = `https://raw.githubusercontent.com/google/material-design-icons/master/symbols/web/${icon}/materialsymbolsrounded/${filename}.svg`;
 </script>
 
-<span style:opacity {style} class="material-symbols-rounded root">{icon}</span>
+<img class="root" {src} alt={icon} />
 
 <style>
     .root {
-        font-size: 1em;
         line-height: 1;
         overflow: hidden;
         vertical-align: -0.15em;
